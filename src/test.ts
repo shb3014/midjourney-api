229492cb350e4d4dfc875392fe4c1c833e6b284f
Gaoran Sun <33003517+shb3014@users.noreply.github.com>
2023-06-19 21:43:29 +0800

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -15,15 +15,15 @@ async function main() {
         // ChannelId: <string>"1111293075222954005",
         // // SalaiToken: <string>"MTAxNzk1NDM4Mjg5NTEzMjY4Mg.GaEFTH.dKjjJSYTC-ij50aQwWY8H7doafaA-hnNiiscK8",
         // SalaiToken: <string>"MTAxNzk1NDM4Mjg5NTEzMjY4Mg.G9gdYZ.uj9MsCXdydJYVPc0dl23KdsrrQDIW-UAgQCML8",
-        ServerId: <string>"1118910989354672241",
-        ChannelId: <string>"1118910989920911432",
+        ServerId: <string>"1120325892808396882",
+        ChannelId: <string>"1120325892808396885",
         // SalaiToken: <string>"MTAxNzk1NDM4Mjg5NTEzMjY4Mg.GaEFTH.dKjjJSYTC-ij50aQwWY8H7doafaA-hnNiiscK8",
         SalaiToken: <string>"MTAxMzU5MTIzMTM0NDk0MzEyNA.GNCg1l.wbNZjkW_kxLARSemJRXCF6jEGqtx31k8QSv-lE",
         Debug: true,
         Ws: true,
         Proxy: "http://127.0.0.1:10809"
     });
-    const msg = await client.Imagine("A giant space shipyard", (uri: string) => {
+    const msg = await client.Imagine("A giant space shipyard around moon", (uri: string) => {
         console.log("loading", uri);
     });
     console.log({msg});
